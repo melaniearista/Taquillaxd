644e7cdbb6529688c2b2811eacee5bef4a3a0e83
melaniearista <47357700+melaniearista@users.noreply.github.com>
2019-02-27 18:02:57 +0100

    Add files via upload

---
+++ asientos.js
@@ -0,0 +1,16 @@
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+/*
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+*/
